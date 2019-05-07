52ecb1fbcedef8f39a7eb26df84d94981a24d2be
xxx <toyoungtime@gmail.com>
2019-05-07 21:59:59 +0800

    啊呵

--- ArrivingStation/app/src/main/java/com/tyt/arrivingstation/MainActivity.java
+++ ArrivingStation/app/src/main/java/com/tyt/arrivingstation/MainActivity.java
@@ -1,23 +1,65 @@
 package com.tyt.arrivingstation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Filter;
+import android.widget.Toast;
 
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
-import com.baidu.location.LocationClient;
-import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.Overlay;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.geocode.GeoCodeOption;
+import com.baidu.mapapi.search.geocode.GeoCodeResult;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
+import com.baidu.mapapi.search.sug.OnGetSuggestionResultListener;
+import com.baidu.mapapi.search.sug.SuggestionResult;
+import com.baidu.mapapi.search.sug.SuggestionSearch;
+import com.baidu.mapapi.search.sug.SuggestionSearchOption;
+import com.baidu.mapapi.utils.CoordinateConverter;
+import com.baidu.mapapi.utils.DistanceUtil;
+import com.tyt.arrivingstation.service.LocationService;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    public LocationClient mLocationClient = null;
+import static com.baidu.mapapi.utils.CoordinateConverter.CoordType.BD09LL;
+
+public class MainActivity extends AppCompatActivity implements OnGetGeoCoderResultListener, OnGetSuggestionResultListener {
+    private static final String TAG = "mainactivitytag";
+    //    public LocationClient mLocationClient = null;
     private MapView mMapView;
     private BaiduMap mBaiduMap;
-    //    private MyLocationListener myListener = new MyLocationListener();
-//    private GeoCoder mSearch;
+    private GeoCoder mSearch;
+    private BDLocation curLocation;
+    private BDLocation desLocation;
+    private double distance = -1;
+    private LocationService locationService;
+    private SuggestionSearch mSuggestionSearch;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> countries;
+    private Overlay overlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,61 +72,185 @@ public class MainActivity extends AppCompatActivity {
         mBaiduMap.setMyLocationEnabled(true);
 
 
-        //定位初始化
-        mLocationClient = new LocationClient(this);
+        locationService = new LocationService(this);
+        locationService.registerListener(new MyLocationListener());
+        locationService.start();
 
-        //通过LocationClientOption设置LocationClient相关参数
-        LocationClientOption option = new LocationClientOption();
-        option.setOpenGps(true); // 打开gps
-        option.setCoorType("bd09ll"); // 设置坐标类型cd
-        option.setScanSpan(1000);
-
-        //设置locationClientOption
-        mLocationClient.setLocOption(option);
-        //注册LocationListener监听器
-        MyLocationListener myLocationListener = new MyLocationListener();
-        mLocationClient.registerLocationListener(myLocationListener);
-        //开启地图定位图层
-        mLocationClient.start();
+        mSuggestionSearch = SuggestionSearch.newInstance();
+        mSuggestionSearch.setOnGetSuggestionResultListener(this);
 
 
-        // TODO: 2018/9/19 检索位置编码
-        //1.创建地理编码检索实例；
-//        mSearch = GeoCoder.newInstance();
-//        //2.创建地理编码检索监听者；
-//        OnGetGeoCoderResultListener listener = new OnGetGeoCoderResultListener() {
-//            public void onGetGeoCodeResult(GeoCodeResult result) {
-//                if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
-//                    //没有检索到结果
-//                }
-//                Log.e("tyt", "latitude: " + result.getLocation().latitude);
-//                Log.e("tyt", "longitude: " + result.getLocation().longitude);
-//                Log.e("tyt", "truth: 40.0030650000,116.4870030000");
-//                //获取地理编码结果
-//            }
-//
-//            @Override
-//            public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
-//                if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {//没有找到检索结果
-//                }//获取反向地理编码结果
-//            }
-//        };
-//
-//        //3。设置地理编码检索监听者；
-//        mSearch.setOnGetGeoCodeResultListener(listener);
-//        //4.发起地理编码检索；
-//        mSearch.geocode(new GeoCodeOption().city("北京").address("地铁望京东站"));
+        final AutoCompleteTextView auto = (AutoCompleteTextView) findViewById(R.id.auctv);
+        auto.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable editable) {
+                if (adapter != null) {
+                    adapter.notifyDataSetChanged();
+                    adapter.notifyDataSetInvalidated();
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mSuggestionSearch.requestSuggestion(new SuggestionSearchOption().city("北京").keyword(s.toString()).citylimit(true));
+            }
+        });
+
+        auto.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int index, long arg3) {
+                getLongLan(countries.get(index) != null ? countries.get(index) : "");
+            }
+        });
+
+
+        countries = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries) {
+            private Filter f;
+
+            @NonNull
+            @Override
+            public Filter getFilter() {
+                if (f == null) {
+                    f = new Filter() {
+                        @Override
+                        protected synchronized FilterResults performFiltering(CharSequence c) {
+                            ArrayList<Object> suggestions = new ArrayList<Object>();
+                            for (String adr : countries) {
+                                suggestions.add(adr);
+                            }
+                            Log.d("test", countries.toString());
+                            FilterResults filterResults = new FilterResults();
+                            filterResults.values = suggestions;
+                            filterResults.count = suggestions.size();
+                            return filterResults;
+                        }
+
+                        @Override
+                        protected synchronized void publishResults(CharSequence c, FilterResults results) {
+                            if (results.count > 0) {
+                                adapter.notifyDataSetChanged();
+                            } else {
+                                adapter.notifyDataSetInvalidated();
+                            }
+                        }
+                    };
+                }
+                return f;
+            }
+        };
+        auto.setAdapter(adapter);
+
     }
 
-    public class MyLocationListener extends BDAbstractLocationListener {
 
+    /**
+     * 测距
+     */
+    private double getDistance() {
+        LatLng curLatLng = new LatLng(curLocation.getLatitude(), curLocation.getLongitude());
+        LatLng desLatLng = new LatLng(desLocation.getLatitude(), desLocation.getLongitude());
+
+        //转换坐标
+        CoordinateConverter converter = new CoordinateConverter().from(BD09LL).coord(desLatLng);
+        LatLng desLatLng1 = converter.convert();
+
+        addPin(desLatLng1);
+        double distance = DistanceUtil.getDistance(curLatLng, desLatLng);
+        Log.e(TAG, "距离: " + distance);
+        return distance;
+    }
+
+    private void addPin(LatLng desLatLng) {
+        if (overlay != null)
+            overlay.remove();
+        //定义Maker坐标点
+//        LatLng point = new LatLng(39.944251, 116.494996);
+        //构建Marker图标
+        BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.mipmap.pin);
+        //构建MarkerOption，用于在地图上添加Marker
+        MarkerOptions option = new MarkerOptions()
+                .position(desLatLng) //必传参数
+                .icon(bitmap) //必传参数
+                .draggable(true)
+                //设置平贴地图，在地图中双指下拉查看效果
+                .flat(true)
+                .alpha(0.5f);
+        //在地图上添加Marker，并显示
+        option.animateType(MarkerOptions.MarkerAnimateType.jump);
+        overlay = (Marker)mBaiduMap.addOverlay(option);
+    }
+
+
+    /**
+     * 检索位置编码
+     */
+    private void getLongLan(String s) {
+        //1.创建地理编码检索实例；
+        mSearch = GeoCoder.newInstance();
+        //2.创建地理编码检索监听者；
+        //3。设置地理编码检索监听者；
+        mSearch.setOnGetGeoCodeResultListener(this);
+        //4.发起地理编码检索；
+        mSearch.geocode(new GeoCodeOption().city("北京").address(s));
+    }
+
+    /**
+     * 位置建议
+     */
+    @Override
+    public void onGetSuggestionResult(SuggestionResult suggestionResult) {
+        synchronized (this) {
+            countries.clear();
+            adapter.notifyDataSetChanged();
+            List<SuggestionResult.SuggestionInfo> allSuggestions = suggestionResult.getAllSuggestions();
+            if (allSuggestions != null) {
+                for (SuggestionResult.SuggestionInfo info : allSuggestions) {
+                    if (info != null)
+                        countries.add(info.key);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 定位结束
+     */
+    @Override
+    public void onGetGeoCodeResult(GeoCodeResult geoCodeResult) {
+        if (geoCodeResult == null || geoCodeResult.error != SearchResult.ERRORNO.NO_ERROR) {//没有找到检索结果
+            Toast.makeText(this, "无法找到目的地", Toast.LENGTH_SHORT).show();
+        } else if (geoCodeResult.getLocation() != null) {
+            desLocation = new BDLocation();
+            desLocation.setLatitude(geoCodeResult.getLocation().latitude);
+            desLocation.setLongitude(geoCodeResult.getLocation().longitude);
+            double distance = getDistance();
+            Toast.makeText(this, "相距" + (int) distance + "米", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "desLocation: " + desLocation.getLatitude() + "--" + desLocation.getLongitude());
+        }
+    }
+
+    @Override
+    public void onGetReverseGeoCodeResult(ReverseGeoCodeResult reverseGeoCodeResult) {
+
+    }
+
+    private class MyLocationListener extends BDAbstractLocationListener {
 
         @Override
         public void onReceiveLocation(BDLocation location) {
+            curLocation = location;
             //mapView 销毁后不在处理新接收的位置
             if (location == null || mMapView == null) {
                 return;
             }
+            Log.e(TAG, "curLocation: " + location.getLatitude() + "--" + location.getLongitude());
+            // 此处设置开发者获取到的方向信息，顺时针0-360
             MyLocationData locData = new MyLocationData.Builder()
                     .accuracy(location.getRadius())
                     // 此处设置开发者获取到的方向信息，顺时针0-360
@@ -94,10 +260,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     @Override
     protected void onResume() {
         super.onResume();
         mMapView.onResume();
+
+        new CountDownTimer(20000, 2000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                /*if (curLocation != null && desLocation != null) {
+                    if (distance == -1) {
+                        distance = getDistance();
+                        Toast.makeText(MainActivity.this, "距离:" + distance, Toast.LENGTH_SHORT).show();
+                    }
+                } else if (desLocation == null) {
+//                    getLongLan("");
+                }*/
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
     }
 
     @Override
@@ -108,12 +295,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        mLocationClient.stop();
         mBaiduMap.setMyLocationEnabled(false);
+//        mLocationClient.stop();
         mMapView.onDestroy();
         mMapView = null;
+        mSearch.destroy();
+        locationService.unregisterListener();
         super.onDestroy();
-        //5.释放地理编码检索实例；
-//        mSearch.destroy();
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_HOME) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            startActivity(intent);
+            return moveTaskToBack(true);
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
--- ArrivingStation/app/src/main/java/com/tyt/arrivingstation/service/LocationService.java
+++ ArrivingStation/app/src/main/java/com/tyt/arrivingstation/service/LocationService.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
-import com.baidu.location.LocationClientOption.LocationMode;
 
 /**
  * 
@@ -16,6 +15,7 @@ public class LocationService {
 	private LocationClient client = null;
 	private LocationClientOption mOption,DIYoption;
 	private Object  objLock = new Object();
+    private BDAbstractLocationListener listener;
 
 	/***
 	 * 
@@ -37,6 +37,7 @@ public class LocationService {
 	 */
 	
 	public boolean registerListener(BDAbstractLocationListener listener){
+        this.listener = listener;
 		boolean isSuccess = false;
 		if(listener != null){
 			client.registerLocationListener(listener);
@@ -45,7 +46,7 @@ public class LocationService {
 		return  isSuccess;
 	}
 	
-	public void unregisterListener(BDAbstractLocationListener listener){
+	public void unregisterListener(){
 		if(listener != null){
 			client.unRegisterLocationListener(listener);
 		}
@@ -74,10 +75,10 @@ public class LocationService {
 	public LocationClientOption getDefaultLocationClientOption(){
 		if(mOption == null){
 			mOption = new LocationClientOption();
-			mOption.setLocationMode(LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
 			mOption.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
-			mOption.setScanSpan(3000);//可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
-		    mOption.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+			mOption.setScanSpan(1000);//可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
+		   /* mOption.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+			mOption.setLocationMode(LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
 		    mOption.setIsNeedLocationDescribe(true);//可选，设置是否需要地址描述
 		    mOption.setNeedDeviceDirect(false);//可选，设置是否需要设备方向结果
 		    mOption.setLocationNotify(false);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
@@ -86,7 +87,7 @@ public class LocationService {
 		    mOption.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
 		    mOption.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
 			mOption.setOpenGps(true);//可选，默认false，设置是否开启Gps定位
-		    mOption.setIsNeedAltitude(false);//可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
+		    mOption.setIsNeedAltitude(false);*///可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
 		 
 		}
 		return mOption;
