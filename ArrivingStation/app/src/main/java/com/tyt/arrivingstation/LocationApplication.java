76d1d52a5df6885aa68c81a8df697ed1398f4196
xxx <toyoungtime@gmail.com>
2019-05-07 10:45:46 +0800

    no message

--- ArrivingStation/app/src/main/java/com/tyt/arrivingstation/LocationApplication.java
+++ ArrivingStation/app/src/main/java/com/tyt/arrivingstation/LocationApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.app.Service;
 import android.os.Vibrator;
 
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
 import com.tyt.arrivingstation.service.LocationService;
 
 /**
@@ -17,7 +19,12 @@ public class LocationApplication extends Application {
     public void onCreate() {
         super.onCreate();
         locationService = new LocationService(getApplicationContext());
-        mVibrator =(Vibrator)getApplicationContext().getSystemService(Service.VIBRATOR_SERVICE);
-//        SDKInitializer.initialize(getApplicationContext());
+        mVibrator = (Vibrator) getApplicationContext().getSystemService(Service.VIBRATOR_SERVICE);
+
+
+        SDKInitializer.initialize(this);
+        //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
+        //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
+        SDKInitializer.setCoordType(CoordType.BD09LL);
     }
 }
--- ArrivingStation/app/src/main/java/com/tyt/arrivingstation/MainActivity.java
+++ ArrivingStation/app/src/main/java/com/tyt/arrivingstation/MainActivity.java
@@ -3,12 +3,20 @@ package com.tyt.arrivingstation;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
 
 public class MainActivity extends AppCompatActivity {
 
     public LocationClient mLocationClient = null;
-//    private MyLocationListener myListener = new MyLocationListener();
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    //    private MyLocationListener myListener = new MyLocationListener();
 //    private GeoCoder mSearch;
 
     @Override
@@ -16,20 +24,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mMapView = (MapView) findViewById(R.id.bmapView);
+
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
 
 
+        //定位初始化
+        mLocationClient = new LocationClient(this);
 
+        //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型cd
+        option.setScanSpan(1000);
 
-
-
-
-
-
-
-
-
-
-
+        //设置locationClientOption
+        mLocationClient.setLocOption(option);
+        //注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+        //开启地图定位图层
+        mLocationClient.start();
 
 
         // TODO: 2018/9/19 检索位置编码
@@ -60,9 +76,42 @@ public class MainActivity extends AppCompatActivity {
 //        mSearch.geocode(new GeoCodeOption().city("北京").address("地铁望京东站"));
     }
 
+    public class MyLocationListener extends BDAbstractLocationListener {
+
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
 
     @Override
     protected void onDestroy() {
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
         super.onDestroy();
         //5.释放地理编码检索实例；
 //        mSearch.destroy();
